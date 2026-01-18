bd6da134866fd8181a41d1b043ff9cbe154cc58e
qixiangme <erang903@khu.ac.kr>
2026-01-18 17:34:10 +0900

    fix: 임포트 페키지 누락 해결

--- src/main/kotlin/com/fsp/coreserver/user/User.kt
+++ src/main/kotlin/com/fsp/coreserver/user/User.kt
@@ -1,3 +1,12 @@
+package com.fsp.coreserver.user
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
 @Entity
 @Table(name = "users")
 class User(
